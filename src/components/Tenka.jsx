7993db2e3015981ee5723bf1e89a32a930939aa9
website-HIP <phpdimas378@gmail.com>
2024-09-12 12:55:33 +0700

    biru

--- src/components/Tenka.jsx
+++ src/components/Tenka.jsx
@@ -17,7 +17,7 @@ const Tenka = () => {
     <div id="tentangkami" className="w-full flex items-center justify-center bg-cover bg-center">
       <main
 
-        className="flex-grow text-black p-10 flex h-[100vh] justify-center items-center"
+        className="flex-grow text-black p-10 flex  justify-center items-center"
         style={{ backgroundImage: `url(${Logo})` }}
       >
         {/* Kolom Kiri */}
@@ -33,9 +33,9 @@ const Tenka = () => {
         </div>
 
         {/* Kolom Kanan */}
-        <div id="body" className="w-1/2" data-aos="fade-right"> {/* Animasi ketika masuk dari kanan */}
-          <h1 id="intro" className="text-5xl font-bold mb-[2rem]">INTRODUCTION</h1>
-          <div id="bar" className="w-56 h-1 bg-yellow-500 mb-[2rem]"></div>
+        <div id="body" className="w-[50%] mt-[80px]" data-aos="fade-right"> {/* Animasi ketika masuk dari kanan */}
+          <h1 id="intro" className="text-5xl font-bold mb-[1rem]">INTRODUCTION</h1>
+          <div id="bar" className="w-56 h-1 bg-yellow-500 mb-[1rem]"></div>
           <p className="mb-4">
             To continue provide quality services, PT. Haekal Inti Pratama will continue to maintain the company's principles by carrying out <br id="1" />effective control and management in every product and services we offer. PT. Haekal Inti Pratama as a leading General Contractor, <br id="1" /> Safety Equipment, and Medical Supplier (Hyperbaric Chamber & Marine Services) we always provides the best services and products to meet partner needs. We offer integrity, <br id="1" />skill and flexibility through the best combination of budget and speed. To prove our competitiveness, <br id="1" /> we continuously improve our service and selection of quality product. To all partners and clients, we are committed to maintaining the <br id="1" /> satisfaction of PT. Haekal Inti Pratama partners.
             This company <br id="1" />profile was created as a basis for <br id="1" />consideration, offers, cooperation, and product information <br id="1" /> from our company. Finally, we thank you for <br id="1" />y the opportunity to receive and <br id="1" />read this company profile and we hope for your cooperation.
